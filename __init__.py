ab6d09bed99a97ccdd17d37e0da5869ed210580f
Christian Fobel <christian@fobel.net>
2018-01-14 21:26:16 -0500

    chore(home): transfer ownership to Sci-Bots

--- __init__.py
+++ __init__.py
@@ -1,21 +1,3 @@
-"""
-Copyright 2016 Christian Fobel
-
-This file is part of user_prompt_plugin.
-
-user_prompt_plugin is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-user_prompt_plugin is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with user_prompt_plugin.  If not, see <http://www.gnu.org/licenses/>.
-"""
 import logging
 import json
 
